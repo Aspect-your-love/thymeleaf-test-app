52899db3c50d96ce5677a4a4c5ff20e52da3a06b
Aspect-you-love <alekseev.feniks53@yandex.ru>
2025-03-05 23:49:15 +0700

    first init application

---
+++ src/main/java/net/aspect/education/thymeleaftestapp/ThymeleafTestAppApplication.java
@@ -0,0 +1,13 @@
+package net.aspect.education.thymeleaftestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/aspect/education/thymeleaftestapp/ThymeleafTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.aspect.education.thymeleaftestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
