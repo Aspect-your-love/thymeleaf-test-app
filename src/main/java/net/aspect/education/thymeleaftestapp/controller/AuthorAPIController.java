5512562852ab1d15999656f364883edf27c3fb10
Aspect-your-love <kras.developer@yandex.ru>
2025-03-24 12:12:13 +0700

    Change mapping Book controller. Add Author API controller

---
+++ src/main/java/net/aspect/education/thymeleaftestapp/controller/AuthorAPIController.java
@@ -0,0 +1,9 @@
+package net.aspect.education.thymeleaftestapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestContoller
+@RequestMapping("/api/author")
+public class AuthorAPIController {
+}
--- src/main/java/net/aspect/education/thymeleaftestapp/controller/BookAPIController.java
+++ src/main/java/net/aspect/education/thymeleaftestapp/controller/BookAPIController.java
@@ -1,21 +1,16 @@
 package net.aspect.education.thymeleaftestapp.controller;
 
-import jakarta.annotation.PostConstruct;
 import net.aspect.education.thymeleaftestapp.db.dto.BookDTO;
 import net.aspect.education.thymeleaftestapp.db.dto.Mapper;
-import net.aspect.education.thymeleaftestapp.db.entity.Author;
-import net.aspect.education.thymeleaftestapp.db.entity.Book;
-import net.aspect.education.thymeleaftestapp.db.service.authorservice.AuthorService;
 import net.aspect.education.thymeleaftestapp.db.service.bookservice.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
-public class BaseControllerBookAnnotation {
+@RequestMapping("/api/book")
+public class BookAPIController {
 
 
     private final BookService bookService;
@@ -23,28 +18,28 @@ public class BaseControllerBookAnnotation {
     private Mapper mapper = new Mapper();
 
     @Autowired
-    public BaseControllerBookAnnotation(BookService bookService){
+    public BookAPIController(BookService bookService){
         this.bookService = bookService;
     }
 
     //9 TODO: Исправить данный метод. Сделать так, чтобы возвращался список DTO.
-    @GetMapping("/books")
+    @GetMapping("/allBooks")
     public List<BookDTO> showAllBooks(){
         return bookService.getAllBook();
     }
 
-    @GetMapping("/book/{id}")
+    @GetMapping("/getBook/{id}")
     public BookDTO showBookById(@PathVariable("id") int id){
         return bookService.getBookById(id);
     }
 
-    @PostMapping("/book")
+    @PostMapping("/addBook")
     public void addBook(@RequestBody BookDTO book){
         bookService.saveBook(book);
     }
 
     /// Данный метод удаляет книгу по заданному ID
-    @DeleteMapping("/book/{id}")
+    @DeleteMapping("/deleteBook/{id}")
     public void deleteBook(@PathVariable int id){
         bookService.deleteBookById(id);
     }
