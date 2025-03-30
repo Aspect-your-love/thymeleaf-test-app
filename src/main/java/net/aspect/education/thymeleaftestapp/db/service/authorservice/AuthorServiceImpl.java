3efce93720dd658b4d213589c00f821b377bdec7
Aspect-your-love <kras.developer@yandex.ru>
2025-03-30 20:31:58 +0700

    Add methods

--- src/main/java/net/aspect/education/thymeleaftestapp/db/service/authorservice/AuthorServiceImpl.java
+++ src/main/java/net/aspect/education/thymeleaftestapp/db/service/authorservice/AuthorServiceImpl.java
@@ -1,9 +1,11 @@
 package net.aspect.education.thymeleaftestapp.db.service.authorservice;
 
+import jakarta.transaction.Transactional;
 import net.aspect.education.thymeleaftestapp.db.dto.AuthorDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
@@ -11,7 +13,38 @@ public class AuthorServiceImpl implements AuthorService {
      * Получение всех авторов
      * через DTO списком*/
     @Override
+    @Transactional
     public List<AuthorDTO> getAll() {
         return List.of();
+    }
+
+    @Override
+    @Transactional
+    public Optional<AuthorDTO> getById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    @Transactional
+    public Optional<AuthorDTO> getByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    @Transactional
+    public AuthorDTO addAuthor(AuthorDTO author) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void deleteAuthor(int id) {
+
+    }
+
+    @Override
+    @Transactional
+    public AuthorDTO updateAuthor(AuthorDTO authorDTO) {
+        return null;
     }
 }
