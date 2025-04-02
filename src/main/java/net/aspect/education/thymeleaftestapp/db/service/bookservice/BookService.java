77ab4a24617a88ab6cc3355a05938acc7e3f8502
Aspect-your-love <kras.developer@yandex.ru>
2025-04-02 17:16:02 +0700

    Add methods

--- src/main/java/net/aspect/education/thymeleaftestapp/db/service/bookservice/BookService.java
+++ src/main/java/net/aspect/education/thymeleaftestapp/db/service/bookservice/BookService.java
@@ -1,5 +1,15 @@
 package net.aspect.education.thymeleaftestapp.db.service.bookservice;
 
+import net.aspect.education.thymeleaftestapp.db.dto.BookDTO;
+import net.aspect.education.thymeleaftestapp.db.entity.Book;
+
+import java.util.List;
+
 public interface BookService {
 
+    public List<BookDTO> getAll();
+
+    BookDTO getById(int i);
+
+    BookDTO getByName(String name);
 }
